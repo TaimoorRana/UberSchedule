b829458466e2d563c6dff1cdaf3dc461bdf18ecc
Taimoor <taimoorrana1@hotmail.com>
2015-03-05 22:09:10 -0500

    trying to make the login work - not successful

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -5,6 +5,10 @@ class LoginController < ApplicationController
   end
 
   def login
-
+  @user = User.where(:first_name => params[:username])
+  @password = @user.password
+    if @user.password == params[:password]
+      redirect_to :controller => 'Schedule', :action => 'schedule'
+    end
   end
 end
