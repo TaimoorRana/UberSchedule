6a22abcee7db9a11285321f011a4cb700547063e
Taimoor <taimoorrana1@hotmail.com>
2015-03-05 19:08:58 -0500

    Added a user database

--- db/migrate/20150305234021_create_users.rb
+++ db/migrate/20150305234021_create_users.rb
@@ -5,7 +5,6 @@ class CreateUsers < ActiveRecord::Migration
       t.string 'last_name' , :limit => 40
       t.string 'password' , :limit => 20
       t.string 'email', :limit => 30
-
       t.timestamps null: false
     end
   end
