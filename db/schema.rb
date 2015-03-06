925293ca33e2fac37c9fe249cb92d0334a96e6fc
Taimoor <taimoorrana1@hotmail.com>
2015-03-06 15:12:06 -0500

    Changed variables to match the database

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150306170001) do
+ActiveRecord::Schema.define(version: 20150306195957) do
 
   create_table "courses", force: :cascade do |t|
     t.datetime "created_at",                                             null: false
@@ -23,12 +23,11 @@ ActiveRecord::Schema.define(version: 20150306170001) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "first_name",      limit: 40
-    t.string   "last_name",       limit: 40
     t.string   "email",           limit: 30
     t.datetime "created_at",                  null: false
     t.datetime "updated_at",                  null: false
     t.string   "password_digest", limit: 255
+    t.string   "username",        limit: 255
   end
 
 end
