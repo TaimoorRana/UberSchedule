6dc90c8d10b5228daebb02da137286ee930036f9
Taimoor <taimoorrana1@hotmail.com>
2015-03-06 15:13:48 -0500

    nothing changed

--- db/migrate/20150306170001_add_password_digest_to_users.rb
+++ db/migrate/20150306170001_add_password_digest_to_users.rb
@@ -1,6 +1,7 @@
 class AddPasswordDigestToUsers < ActiveRecord::Migration
   def up
     remove_column 'users', 'password'
+    add_column 'users', 'username', :string
     add_column 'users', 'password_digest' , :string
   end
 
