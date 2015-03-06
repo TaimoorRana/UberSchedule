f168d90d93d304fea9d12d45ecdd8710d34a5d67
Taimoor <taimoorrana1@hotmail.com>
2015-03-06 12:33:55 -0500

    got the login working

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -5,10 +5,25 @@ class LoginController < ApplicationController
   end
 
   def login
-  @user = User.where(:first_name => params[:username])
-  @password = @user.password
-    if @user.password == params[:password]
-      redirect_to :controller => 'Schedule', :action => 'schedule'
+
+    if params[:username].present? && params[:password].present?
+      found_user = User.where(first_name: params[:username]).first
+      if found_user
+        authorized_user = found_user.authenticate(params[:password])
+      end
+      if authorized_user
+        flash[:notice] = 'you are logged in'
+        redirect_to :controller => 'schedule', action: 'schedule'
+      else
+        flash[:notice] = 'Invalid User'
+        redirect_to action: 'index'
+      end
     end
+
+    #@user = User.where(:first_name => params[:username])
+   # @password = @user.password
+   # if @user.password == params[:password]
+    #  redirect_to :controller => 'Schedule', :action => 'schedule'
+    #end
   end
 end
