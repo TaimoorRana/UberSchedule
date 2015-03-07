e2758e4e9456b20439390ca42d82a2d5ad2a478e
Taimoor <taimoorrana1@hotmail.com>
2015-03-06 20:07:19 -0500

    add a method to verify if the user session is valid

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,14 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  def confirm_logged_in
+    if session[:user_id]
+      return true
+    else
+      redirect_to controller: 'login', action: 'index'
+      return false
+    end
+  end
+
 end
