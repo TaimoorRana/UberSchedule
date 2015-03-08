497f5cfc1b563b7fc69d8c653eb71d9752bd6d02
Taimoor <taimoorrana1@hotmail.com>
2015-03-08 15:24:27 -0400

    we can now delete courses

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -5,4 +5,10 @@ class UserController < ApplicationController
     flash[:notice] = "#{course.name} was added"
     redirect_to controller: 'course',action: 'index'
   end
+
+  def remove_course
+    course = $authorized_user.courses.find(params[:id])
+    $authorized_user.courses.delete(course)
+    redirect_to controller: 'schedule', action: 'schedule'
+  end
 end
