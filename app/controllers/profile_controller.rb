9927be787b5f53b5cb339b9dcfdbeaf454f1e715
Richard Grand'Maison <rich.grandmaison@gmail.com>
2015-03-14 19:14:35 -0400

    Added if statements to handle nil arguments passed in the params.

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -5,29 +5,41 @@ class ProfileController < ApplicationController
   end
 
   def updateProfile
-    loyola = params[:loyola]
-    split = params[:split]
-    major = params[:major]
+
+    (params[:loyola] == "Yes") ? loyola = 1 : loyola = 0
+    (params[:split] == "Yes") ? split = 1 : split = 0
+
     status = params[:status]
 
-    mondayAM = params[:mondayAm]
-    mondayPM = params[:mondayPm]
-    tuedayAM = params[:tuesdayAm]
-    tuedayPM = params[:tuesdayPm]
-    wednesdayAM = params[:wednesdayAm]
-    wednesdayPM = params[:wednesdayPm]
-    thursdayAM = params[:thursdayAm]
-    thursdayPM = params[:thursdayPm]
-    fridayAM = params[:fridayAm]
-    fridayPM = params[:fridayPm]
+    (params[:mondayAm] == nil) ? mondayAM = 0 : mondayAM = 1
+    (params[:mondayPm] == nil) ? mondayPM = 0 : mondayPM = 1
+    (params[:tuesdayAm] == nil) ? tuesdayAM = 0 : tuesdayAM = 1
+    (params[:tuesdayPm] == nil) ? tuesdayPM = 0 : tuesdayPM = 1
+    (params[:wednesdayAm] == nil) ? wednesdayAM = 0 : wednesdayAM = 1
+    (params[:wednesdayPm] == nil) ? wednesdayPM = 0 : wednesdayPM = 1
+    (params[:thursdayAm] == nil) ? thursdayAM = 0 : thursdayAM = 1
+    (params[:thursdayPm] == nil) ? thursdayPM = 0 : thursdayPM = 1
+    (params[:fridayAm] == nil) ? fridayAM = 0 : fridayAM = 1
+    (params[:fridayPm] == nil) ? fridayPM = 0 : fridayPM = 1
 
-    name = $authorized_student.firstname
+
     id = $authorized_student.id
+    student = Student.find(id)
+    preference = student.preference
 
-
-
-    flash[:notice] = "mondayAM = #{mondayAM}"
-
+    preference.update_attribute(:loyola, loyola)
+    preference.update_attribute(:status, status)
+    preference.update_attribute(:split, split)
+    preference.update_attribute(:monday_am, mondayAM)
+    preference.update_attribute(:monday_pm, mondayPM)
+    preference.update_attribute(:tuesday_am, tuesdayAM)
+    preference.update_attribute(:tuesday_pm, tuesdayPM)
+    preference.update_attribute(:wednesday_am, wednesdayAM)
+    preference.update_attribute(:wednesday_pm, wednesdayPM)
+    preference.update_attribute(:thursday_am, thursdayAM)
+    preference.update_attribute(:thursday_pm, thursdayPM)
+    preference.update_attribute(:friday_am, fridayAM)
+    preference.update_attribute(:friday_pm, fridayPM)
 
   end
 end
