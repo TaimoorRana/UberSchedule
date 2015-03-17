bfb7895b3818984fae1b8d76c239e66f70cde466
Taimoor <taimoorrana1@hotmail.com>
2015-03-17 19:35:43 -0400

    updated schema

--- db/migrate/20150317040533_create_preferences.rb
+++ db/migrate/20150317040533_create_preferences.rb
@@ -1,7 +1,11 @@
 class CreatePreferences < ActiveRecord::Migration
-  def change
+  def up
     create_table :preferences do |t|
       t.string 'preference'
     end
+  end
+
+  def down
+    drop_table :preferences
   end
 end
