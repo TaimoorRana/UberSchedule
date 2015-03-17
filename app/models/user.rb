b4c65dfcf4ee40c6216dc661ca8b5a0c66fe1a1f
Taimoor <taimoorrana1@hotmail.com>
2015-03-17 14:29:21 -0400

    A student object can now be create upon registration

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,12 @@ class User < ActiveRecord::Base
   has_one :student
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  after_create :create_student
+
+  private
+
+  def create_student
+    Student.create(user_id: self.id, first_name: self.first_name, last_name: self.last_name)
+  end
 end
