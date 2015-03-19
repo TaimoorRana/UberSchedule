e8db56c84c48593f7ee8f4ead58f02127640efe4
Arek Manoukian <manoukian.arek@gmail.com>
2015-03-19 12:56:07 -0400

    Electives list is working

--- app/controllers/sequence_generator_controller.rb
+++ app/controllers/sequence_generator_controller.rb
@@ -5,6 +5,10 @@ before_action :authenticate_user!,:optionSelected,:generateSequence, only: [:Seq
 
   end
 
+  def updateDoneCourses
+
+  end
+
   private
   def generateSequence ## Private Method to generate a hashmap of Core, Required and Obligatory_list_of_courses_to_be_picked_from_list. For use with displaying the sequence.
     user_id = current_user.user_id
@@ -28,8 +32,31 @@ before_action :authenticate_user!,:optionSelected,:generateSequence, only: [:Seq
     else
       @sequence[:general] = Sequence.where(sequence_name: 'general').first
     end
-
+    getElectiveCourses
   end
+
+  def getElectiveCourses
+    if @sequence[:general].nil? ## Get a list of Electives that you can choose from. Return courses in
+      requiredCourses = []
+      electiveSequence = Sequence.where(sequence_name: 'general').first
+      @electiveCourses = []
+      electiveSequence.courses.each do |course|
+        @electiveCourses.append(course)
+      end
+      @sequence[:required].courses.each do |course|
+        requiredCourses.append(course)
+      end
+      @sequence[:one_of].courses.each do |course|
+        requiredCourses.append(course)
+      end
+      @electiveCourses.each do |course|
+        if requiredCourses.include?(course)
+          @electiveCourses.delete(course)
+        end
+      end
+    end
+  end
+
   def optionSelected
     user_id = current_user.user_id
     option = Student.where(user_id: user_id ).first.option
