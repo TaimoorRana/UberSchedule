2560b5855ccbaf4587f45923abd09a46b688be22
Taimoor <taimoorrana1@hotmail.com>
2015-03-28 18:56:32 -0400

    tutorials partially working

--- app/controllers/schedule_controller.rb
+++ app/controllers/schedule_controller.rb
@@ -8,22 +8,24 @@ class ScheduleController < ApplicationController
 
     #@sections = [Course.find(2).sections.first,Course.find(45).sections.first,Course.find(31).sections.first,Course.find(42).sections.find(194)]
     @sections = []
+    @tutorials = []
     #@courses = [Course.find(4), Course.find(45)]
-    @courses = [Course.find(4), Course.find(45), Course.find(31), Course.find(18),Course.find(17), Course.find(32)]
+    @courses = [Course.find(4), Course.find(45), Course.find(31), Course.find(18),Course.find(17)]
     @mondaySections = []
     @tuesdaySections = []
     @wednesdaySections = []
     @thursdaySections = []
     @fridaySections = []
 
-    found = find_all_schedule
-    if found != nil
-      week = separate_sections_according_to_days
-      @mondaySections = week[0]
-      @tuesdaySections = week[1]
-      @wednesdaySections = week[2]
-      @thursdaySections = week[3]
-      @fridaySections = week[4]
+    if find_all_sections != nil
+      find_all_tutorials
+      week_tutorials = separate_sections_according_to_days(@tutorials)
+      week_sections = separate_sections_according_to_days(@sections)
+      @mondaySections = week_sections[0].append(week_tutorials[0].flatten)
+      @tuesdaySections = week_sections[1]
+      @wednesdaySections = week_sections[2].append(week_tutorials[2].flatten)
+      @thursdaySections = week_sections[3]
+      @fridaySections = week_sections[4].append(week_tutorials[4].flatten)
     end
 
 
@@ -32,28 +34,53 @@ class ScheduleController < ApplicationController
 
   end
 
-  def find_all_schedule
+  def find_all_sections
     @all_courses_sections = []
     @courses.each do |course|
       @all_courses_sections.push(course.sections)
     end
-    @possible_schedule = @all_courses_sections.inject(&:product).map(&:flatten)
+    @possible_sections = @all_courses_sections.inject(&:product).map(&:flatten)
     i = 0
-    @sections = @possible_schedule[i]
+    @sections = @possible_sections[i]
 
-    while (find_conflicts != nil && i < @possible_schedule.size - 1 )
+    while (find_conflicts(separate_sections_according_to_days(@sections)) != nil && i < @possible_sections.size - 1 )
       i += 1
-      @sections = @possible_schedule[i]
+      @sections = @possible_sections[i]
     end
-    @conflicts = []
-    if find_conflicts != []
-      @conflicts = find_conflicts
+    @conflicts_sections = []
+    if find_conflicts(separate_sections_according_to_days(@sections)) != []
+      @conflicts_sections = find_conflicts(separate_sections_according_to_days(@sections))
       return nil
     else
       return 1
     end
 
   end
+
+
+  def find_all_tutorials
+    @all_courses_tutorials = []
+    @sections.each do |section|
+      @all_courses_tutorials.push(section.tutorials)
+    end
+    @possible_tutorials = @all_courses_tutorials.inject(&:product).map(&:flatten)
+    i = 0
+    @tutorials = @possible_tutorials[i]
+
+    while (find_conflicts(separate_sections_according_to_days(@tutorials)) != nil && i < @possible_tutorials.size - 1 )
+      i += 1
+      @tutorials = @possible_tutorials[i]
+    end
+
+    @conflicts = []
+    if find_conflicts(separate_sections_according_to_days(@tutorials)) != []
+      @conflicts_tutorials = find_conflicts(separate_sections_according_to_days(@tutorials))
+      return nil
+    else
+      return 1
+    end
+  end
+
 
 
   def registered_courses
@@ -71,12 +98,12 @@ class ScheduleController < ApplicationController
     @sections.push(section)
   end
 
-  def separate_sections_according_to_days
+  def separate_sections_according_to_days(classType)
     #contains sections for every day
     week = [[],[],[],[],[]]
 
     #Sort sections by the day they are given
-    @sections.each do |section|
+    classType.each do |section|
 
       #this boolean is used to ignore multiple 'W' in day_of_each week - issue with seed.rb
       wednesday_added = false
@@ -86,9 +113,9 @@ class ScheduleController < ApplicationController
         #duration of a section
         duration = (Time.parse(section.time_end) - Time.parse(section.time_start))
 
-        #duration is given is seconds so divide by 60 to get minutes and then divide by 15min because that time unit in schedule is every
+        #duration is given in seconds so divide by 60 to get minutes and then divide by 15min because that time unit in schedule is every
         # 15min. This allow to calculate how many rows a sections will span
-        section_row_span = duration/60/15
+        section_row_span = (duration/60/15).ceil
 
         if day == 'M'
           week[0].push([section,section_row_span])
@@ -128,10 +155,9 @@ class ScheduleController < ApplicationController
 
 
   #find conflit between sections, if conflict is found
-  def find_conflicts
+  def find_conflicts (arr)
 
     # get sections
-    arr = separate_sections_according_to_days
     conflict_arr = []
 
     #find conflicts
