e755a7f8b0c0c75803b814d2eba67bc3ed61cf5a
Richard Grand'Maison <rich.grandmaison@gmail.com>
2015-03-29 16:13:02 -0400

    Completely revamped Sequence Builder. Created methods to convert all the useful data into workable arrays of Objects

--- app/controllers/sequence_builder_controller.rb
+++ app/controllers/sequence_builder_controller.rb
@@ -2,131 +2,121 @@ class SequenceBuilderController < ApplicationController
   layout 'general_schedule'
 
   def sequence_builder
+    @log = Logger.new("C:\\Users\\Rich\\RubymineProjects\\UberSchedule\\log.txt")
+    @log.level = Logger::DEBUG
     user_id = current_user.user_id
-    student = Student.where(user_id: user_id).first
-    preferences = student.preferences
+    @student = Student.where(user_id: user_id).first
+
+    preferences = @student.preferences
     preferences.exists?(16) ? @semester_counter = 0 : @semester_counter = 1 # 0 = Fall Entry, 1 = Winter Entry
     preferences.exists?(14) ? allowSummer = true : allowSummer = false #not implemented
     preferences.exists?(15) ? coop = 1 : coop = 0 #not implemented
-    year = 2015
+
     accumulated_credits = 0 #TODO
-    max_credits = 30
+    max_credits = 100
+    year = 2015
     semester = {0 => "Fall", 1 => "Winter", 2 => "Summer"}
 
-    all_sections = Section.all
-    all_fall_sections = all_sections.where(term: "Fall")
-    all_winter_sections = Section.where(term: "Winter")
-    all_summer_sections = Section.where(term: "Summer")
+    @all_sections = Array.new
+    sections_to_array
+
+    @all_fall_sections = Array.new
+    @all_summer_sections = Array.new
+    @all_winter_sections = Array.new
+    sort_sections_by_term
 
     @fall_courses_only = Array.new
     @winter_courses_only = Array.new
-    semester_section = {"Fall" => all_fall_sections, "Winter" => all_winter_sections, "Summer" => all_summer_sections}
+    @courses_offered_in_summer = Array.new
+    courses_given_in_specific_semester
 
-    #prereqs = CoursesPrereq.all
+    @all_prereqs = Array.new
+    all_prereqs_to_array
+
     @completed_courses = Array.new
-    @complete_sequence = Array.new
+    completed_courses_to_array
 
-    #Find courses only given in Fall or Winter
-    previous_course_id = 0
-    all_sections.each do |section|
-      current_course_id = section.course_id
-      current_course = Course.find(current_course_id)
-      if previous_course_id != current_course_id #prevents double checking for same course
-        previous_course_id = current_course_id
-        fall_offered = all_fall_sections.exists?(course_id: current_course_id)
-        winter_offered = all_winter_sections.exists?(course_id: current_course_id)
-        summer_offered = all_summer_sections.exists?(course_id: current_course_id)
-        if summer_offered == false
-          if fall_offered and !winter_offered
-            @fall_courses_only.push(current_course)
-          elsif winter_offered and !fall_offered
-            @winter_courses_only.push(current_course)
-          end
-        end #if summer_size == 0
+    @number_of_direct_dependents = Array.new
+    number_of_direct_dependents
+
+  end #end of def sequence_builder
+
+  # populates the @all_sections array with Section objects
+  def sections_to_array #tested for class type Array and correct output
+    Section.all.each do |section|
+      @all_sections.push(section)
+    end
+  end
+
+  # Filters the Section objects from @all_sections array into small array, according to term
+  def sort_sections_by_term #tested for class type Array and proper sort
+    @all_sections.each do |section|
+      if section.term == "Fall"
+        @all_fall_sections.push(section)
+      elsif section.term == "Winter"
+        @all_winter_sections.push(section)
+      elsif section.term = "Summer"
+        @all_summer_sections.push(section)
       end
-    end # end of do
+    end
+  end
 
-
-    #Generate sequence
-
-    while accumulated_credits < max_credits do
-     @current_semester = Array.new #array to contain course info for this semester
-     #modulo = allowSummer ? 3 : 2 #select the proper modulo: 2 for summer, 3 otherwise
-     modulo = 2
-     current_semester = semester[(@semester_counter).modulo(modulo)] #select current_semester
-     current_semester == "Winter" ? year = year + 1 : year = year
-     semester_year = current_semester + " " + year.to_s
-     @current_semester.push(semester_year)
-
-     available_this_semester = Array.new
-     previous_course_id = 0
-     semester_section[current_semester].each do |section| #loop through all courses given this semester
-        course_id = section.course_id
-        if(previous_course_id != course_id)
-          course = Course.find(course_id) ###---deleted by Adrian
-          previous_course_id = Course.find(course_id)
-          if !@completed_courses.include?(course) #if course not completed
-          #missing_prereq = 0
-          #course_prereqs = prereqs.where(course_id: course_id)
-          #if course_prereqs[0] !=nil #check if course has prereqs
-           # course_prereqs.each do |prereq| #if yes, check if they have been completed
-            #  if !completed_courses.include?(Course.find(prereq.course_id_prereq))
-             #   missing_prereq = 1
-              #  break
-             # end #end of most nested id
-           # end #end of prereqs loop
-         # end #end of if has prereqs
-          #  if missing_prereq == 0
-              available_this_semester.push(course)
-          # end
-         end #end of main if
+  #populates the @fall_courses_only, @winter_courses_only and @courses_offered_in_summer arrays with Course objects
+  def courses_given_in_specific_semester #tested for array types and proper content
+    offered_in_fall = false
+    offered_in_winter = false
+    offered_in_summer = false
+    current_course_id = 1
+    @all_sections.each do |section|
+      if current_course_id != section.course_id
+        if offered_in_summer
+          @courses_offered_in_summer.push(Course.find(current_course_id))
+        else
+          if offered_in_fall and !offered_in_winter
+            @fall_courses_only.push(Course.find(current_course_id))
+            @log.info("added a fall only course")
           end
-      end #end of do
-        semester_classes = (current_semester == "Summer") ? 4 : 5
-        #Add classes from available classes until max_classes is reached
-        #increment credits
-        #add added course to db of completed courses
-        available_length = available_this_semester.length
-        max_classes = [semester_classes, available_length].min
-        classes_counter = 0
-        if available_length != 0
-          while classes_counter < max_classes
-          available_this_semester.each do |course|
-            if current_semester == "Fall" and @fall_courses_only.include?(course) and classes_counter < max_classes
-              @current_semester.push(course_to_string(course))
-              @completed_courses.push(course)
-              accumulated_credits = accumulated_credits + course.credit
-              classes_counter+=1
-            elsif current_semester == "Winter" and @winter_courses_only.include?(course) and classes_counter < max_classes
-              @current_semester.push(course_to_string(course))
-              @completed_courses.push(course)
-              accumulated_credits = accumulated_credits + course.credit
-              classes_counter+=1
-            end
+          if !offered_in_fall and offered_in_winter
+            @winter_courses_only.push(Course.find(current_course_id))
           end
-          end
-            while classes_counter < max_classes
-              available_this_semester.each do |course|
-                if !completed_courses.include?(course)
-                  @current_semester.push(course_to_string(course))
-                  @completed_courses.push(course)
-                  accumulated_credits = accumulated_credits + course.credit
-                  classes_counter+=1
-                  end
-              end
-            end
-
         end
-       @complete_sequence.push(@current_semester)
-       @semester_counter = @semester_counter + 1
-        end #end of until classes_counter == max_classes
+        current_course_id = section.course_id
+        offered_in_fall = false
+        offered_in_winter = false
+        offered_in_summer = false
+      end #end if current_course_id != section.course_id
+        if section.term == "Fall"
+          offered_in_fall = true
+        elsif section.term == "Winter"
+          offered_in_winter = true
+        elsif section.term == "Summer"
+          offered_in_summer = true
+        end
+    end #end do loop
+  end #end of courses_given_only_in_fall_or_winter_plus_summer
+
+  # populates @all_prereqs array with CoursesPrereq objects
+  def all_prereqs_to_array #tested for array type and proper content
+    CoursesPrereq.all.each do |prereq|
+      @all_prereqs.push(prereq)
+    end
   end
 
-  def course_to_string(course)
-    return course.dept + " " + course.number.to_s
+  # populates @complete_courses with the Courses from @student.courses
+  # subsequently used to store Courses added to sequence
+  def completed_courses_to_array #teted for array type and proper content
+    @student.courses.each do |course|
+      @completed_courses.push(course)
+    end
   end
 
-end
+  # Stored the number of direct dependents for a given Course.
+  # The index of the array corresponds to the course_id of the Course.
+  def number_of_direct_dependents #tested for array Type and proper content
+    Course.all.each do |course|
+      @number_of_direct_dependents[course.course_id] =
+          CoursesPrereq.where(course_id_prereq: course.course_id).count
+    end
+  end
 
-
-
+end #end of class
