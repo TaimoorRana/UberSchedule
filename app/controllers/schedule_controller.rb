f7c69dc2ca4841ef4de2f1e08163cfbafa335da8
Taimoor <taimoorrana1@hotmail.com>
2015-03-31 19:55:25 -0400

    fixing the schedule

--- app/controllers/schedule_controller.rb
+++ app/controllers/schedule_controller.rb
@@ -3,13 +3,11 @@ class ScheduleController < ApplicationController
   before_action :authenticate_user!
 
 
-
   def schedule
 
     #@sections = [Course.find(2).sections.first,Course.find(45).sections.first,Course.find(31).sections.first,Course.find(42).sections.find(194)]
     @sections = []
     @tutorials = []
-    #@courses = [Course.find(4), Course.find(45)]
     @courses = [Course.find(1), Course.find(15), Course.find(16),Course.find(3)]
     @mondaySections = []
     @tuesdaySections = []
@@ -18,21 +16,24 @@ class ScheduleController < ApplicationController
     @fridaySections = []
 
     if find_all_sections != nil
-      find_all_tutorials
-      #week_tutorials = separate_sections_according_to_days(@tutorials)
-      week_tutorials = [[],[],[],[],[]]
-      week_sections = separate_sections_according_to_days(@sections)
-      @mondaySections = if week_tutorials[0].flatten != [] then week_sections[0].append(week_tutorials[0].flatten) else week_sections[0] end
-      @tuesdaySections = if week_tutorials[1].flatten != [] then week_sections[1].append(week_tutorials[1].flatten)else week_sections[1] end
-      @wednesdaySections = if week_tutorials[2].flatten != [] then week_sections[2].append(week_tutorials[2].flatten)else week_sections[2] end
-      @thursdaySections = if week_tutorials[3].flatten != [] then week_sections[3].append(week_tutorials[3].flatten)else week_sections[3] end
-      @fridaySections = if week_tutorials[4].flatten != [] then week_sections[4].append(week_tutorials[4].flatten)else week_sections[4] end
-      # week = sort_all_sections_tutorials_labs([@mondaySections,@thursdaySections,@wednesdaySections,@thursdaySections,@fridaySections])
-      # @mondaySections = week[0]
-      # @tuesdaySections = week[1]
-      # @wednesdaySections = week[2]
-      # @thursdaySections = week[3]
-      # @fridaySections = week[4]
+      #find_all_tutorials
+      week_tutorials = separate_sections_according_to_days(@tutorials)
+      #week_tutorials = [[],[],[],[],[]]
+      #@week_sections = sort_all_sections_tutorials_labs(@week_sections)
+      #mixed_sections = week_tutorials.append(week_sections)
+      #mixed_sections = separate_sections_according_to_days(mixed_sections)
+      #week = sort_all_sections_tutorials_labs(mixed_sections)
+      # @mondaySections = if week_tutorials[0].flatten != [] then @week_sections[0].append(week_tutorials[0]).flatten else @week_sections[0] end
+      # @tuesdaySections = if week_tutorials[1].flatten != [] then @week_sections[1].append(week_tutorials[1]).flatten else @week_sections[1] end
+      # @wednesdaySections = if week_tutorials[2].flatten != [] then @week_sections[2].append(week_tutorials[2]).flatten else @week_sections[2] end
+      # @thursdaySections = if week_tutorials[3].flatten != [] then @week_sections[3].append(week_tutorials[3]).flatten else @week_sections[3] end
+      # @fridaySections = if week_tutorials[4].flatten != [] then @week_sections[4].append(week_tutorials[4]).flatten else @week_sections[4] end
+      #week = sort_all_sections_tutorials_labs([@mondaySections,@thursdaySections,@wednesdaySections,@thursdaySections,@fridaySections])
+      @mondaySections = @week_sections[0]
+      @tuesdaySections = @week_sections[1]
+      @wednesdaySections = @week_sections[2]
+      @thursdaySections = @week_sections[3]
+      @fridaySections = @week_sections[4]
     end
 
 
@@ -43,23 +44,26 @@ class ScheduleController < ApplicationController
 
   def find_all_sections
     @all_courses_sections = []
+
     @courses.each do |course|
       @all_courses_sections.push(course.sections)
     end
+
     @possible_sections = @all_courses_sections.inject(&:product).map(&:flatten)
     i = 0
-    @sections = @possible_sections[i]
 
-    while (find_conflicts(separate_sections_according_to_days(@sections)) != nil && i < @possible_sections.size - 1 )
-      i += 1
+    begin
       @sections = @possible_sections[i]
-    end
-    @conflicts_sections = []
-    if find_conflicts(separate_sections_according_to_days(@sections)) != []
-      @conflicts_sections = find_conflicts(separate_sections_according_to_days(@sections))
-      return nil
-    else
+      @week_sections = separate_sections_according_to_days(@sections)
+      @conflicts_sections = find_conflicts(@week_sections)
+      i += 1
+    end while @conflicts_sections != [] && i < (@possible_sections.size - 1)
+
+    if @conflicts_sections == []
+       find_all_tutorials
       return 1
+    else
+      return nil
     end
 
   end
@@ -67,44 +71,44 @@ class ScheduleController < ApplicationController
 
   def find_all_tutorials
     @all_courses_tutorials = []
-    @conflicts_tutorials = []
+
     @sections.each do |section|
       @all_courses_tutorials.push(section.tutorials)
     end
+
     @possible_tutorials = @all_courses_tutorials.inject(&:product).map(&:flatten)
     i = 0
-    @tutorials = @possible_tutorials[i]
 
-    while (find_conflicts(separate_sections_according_to_days(@tutorials)) != nil && i < @possible_tutorials.size - 1 )
-      i += 1
+    begin
       @tutorials = @possible_tutorials[i]
-    end
+      @week_tutorials = separate_sections_according_to_days(@tutorials)
+      merged_sections = merge_sections(@week_sections,@week_tutorials)
+      @conflicts_tutorials = find_conflicts(merged_sections)
+      if @conflicts_tutorials = []
+        @week_sections = sort_all_sections_tutorials_labs(merged_sections)
+      end
+      i += 1
+    end while @conflicts_tutorials != [] && i < (@possible_tutorials.size - 1)
 
-    @conflicts = []
-    if find_conflicts(separate_sections_according_to_days(@tutorials)) != []
-      @conflicts_tutorials = find_conflicts(separate_sections_according_to_days(@tutorials))
-      return nil
-    else
+    if @conflicts_tutorials == []
+      #find_all_tutorials
       return 1
+    else
+      return nil
     end
   end
 
-
-
-  def registered_courses
-
-    @courses = current_user.student.courses
-    @totalCredits = 0
-
-    @courses.each do |course|
-      @totalCredits += course.credit
+  def merge_sections(arr1,arr2)
+    arr_merged = [[],[],[],[],[]]
+    arr1.each_with_index do |useless,i|
+      #arr_merged[i] = arr1[i].append(arr2[i]).flatten
+      arr_merged[i] = arr1[i] + arr2[i]
     end
-
+    return arr_merged
   end
 
-  def add_sections(section)
-    @sections.push(section)
-  end
+
+
 
   def separate_sections_according_to_days(classType)
     #contains sections for every day
@@ -118,34 +122,36 @@ class ScheduleController < ApplicationController
 
       section.day_of_week.to_s.each_char do |day|
 
-        #duration of a section
-        duration = (Time.parse(section.time_end) - Time.parse(section.time_start))
+        if day != '-'
+          #duration of a section
+          duration = (Time.parse(section.time_end) - Time.parse(section.time_start))
 
-        #duration is given in seconds so divide by 60 to get minutes and then divide by 15min because that time unit in schedule is every
-        # 15min. This allow to calculate how many rows a sections will span
-        section_row_span = (duration/60/15).ceil
-        schedule_section = ScheduleSection.new(section,'#F7F7F7',section_row_span)
-        if day == 'M'
-          week[0].push(schedule_section)
-        end
-        if day == 'T'
-          week[1].push(schedule_section)
-        end
-        if day == 'W' && wednesday_added == false
-          week[2].push(schedule_section)
-          wednesday_added = true
-        end
-        if day == 'J'
-          week[3].push(schedule_section)
-        end
-        if day == 'F'
-          week[4].push(schedule_section)
+          #duration is given in seconds so divide by 60 to get minutes and then divide by 15min because that time unit in schedule is every
+          # 15min. This allow to calculate how many rows a sections will span
+          section_row_span = (duration/60/15).ceil
+          schedule_section = ScheduleSection.new(section,'#F7F7F7',section_row_span)
+          if day == 'M'
+            week[0].push(schedule_section)
+
+          elsif day == 'T'
+            week[1].push(schedule_section)
+
+          elsif day == 'W' && wednesday_added == false
+            week[2].push(schedule_section)
+            wednesday_added = true
+          elsif day == 'J'
+            week[3].push(schedule_section)
+
+          elsif day == 'F'
+            week[4].push(schedule_section)
+          else
+          end
         end
       end
     end
 
 
-    sort_all_sections_tutorials_labs(week)
+    #sort_all_sections_tutorials_labs(week)
 
     return week
   end
@@ -156,10 +162,10 @@ class ScheduleController < ApplicationController
 
       day.each_with_index do |schedule_section1,j|
         day.each_with_index do |schedule_section2,k|
-          if (Time.parse(schedule_section1.section.time_start) <=> Time.parse(schedule_section2.section.time_start)) == -1 && j > k
-            temp = schedule_section2
-            day[k] = schedule_section1
-            day[j] = temp
+          if week[i][j] != week[i][k] && (Time.parse(week[i][j].section.time_start) <=> Time.parse(week[i][k].section.time_start)) == -1 && j > k
+            temp = week[i][k]
+            week[i][k] = week[i][j]
+            week[i][j] = temp
           end
         end
       end
@@ -204,13 +210,27 @@ class ScheduleController < ApplicationController
     return conflict_arr.uniq
   end
 
+  def registered_courses
+
+    @courses = current_user.student.courses
+    @totalCredits = 0
+
+    @courses.each do |course|
+      @totalCredits += course.credit
+    end
+
+  end
+
+  def add_sections(section)
+    @sections.push(section)
+  end
+
   class ScheduleSection
-    attr_accessor :section,:section_type,:color,:row_span
+    attr_accessor :section,:color,:row_span
     def initialize(section,color,row_span)
       self.section = section
       self.color = color
       self.row_span = row_span
-      self.section_type = section.class
     end
   end
 
